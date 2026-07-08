44ff9d09db265ca463bb7301a54455212c6afe0e
Glomberg <bazz@bk.ru>
2026-07-08 20:39:03 +0300

    Version 4.0.

--- bazz-callback-widget.php
+++ bazz-callback-widget.php
@@ -6,7 +6,7 @@ Text Domain: bazz-callback-widget
 Domain Path: /languages
 Description: This plugin makes a simple widget for callback on your website.
 Author: Viktor Ievlev
-Version: 3.26
+Version: 4.0
 Author URI: https://viktor-web.ru
 License: GPLv2
 */
